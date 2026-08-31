a20839dd79fc878fea49aaf49f8f1f1745a0447c
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-08-31 10:52:16 -0700

    Output Added

--- src/Day1.tsx
+++ src/Day1.tsx
@@ -29,6 +29,9 @@ export default function Day1(){
                 <Text>Convert</Text>
 
             </Pressable>
+            <Text>{output}</Text>
+            <Text>{typeof output}</Text>
+
         </View>
     )
 };
