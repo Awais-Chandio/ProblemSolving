7e32954d1d916fc52423d2fe6d4dcfbfe0445b54
Awais-Chandio <94171625+Awais-Chandio@users.noreply.github.com>
2026-09-02 14:45:04 +0500

    Day3 Screen Added

---
+++ src/Day3.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react"
+import { View, Text,StyleSheet,Pressable, TextInput } from "react-native"
+
+const Century =(year:number)=>{
+let inputYear = Math.floor(year/100)
+if(year%100!==0){
+    inputYear = inputYear+1;
+}
+return inputYear
+
+}
+export default function Day3(){
+    const[input,setInput] = useState('')
+    const[result,setResult] = useState<number | null>(null)
+
+    const handleFunction =()=>{
+    const year = Number(input)
+    setResult(Century(year));
+    }
+
+
+    return (
+        <View style={Styles.container}>
+            <Text>Day2</Text>
+            <TextInput
+            value={input}
+            onChangeText={setInput}
+            placeholder="Enter any year you will know what's century is "
+            />
+        <Pressable
+        onPress={handleFunction}
+        >
+          <Text>"Show the Century"</Text>
+        </Pressable>
+         <Text>
+               {result},
+            </Text>
+            
+
+        </View>
+    )
+};
+
+const Styles = StyleSheet.create({   
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',      
+    }
+});
+
