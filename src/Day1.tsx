94f0920c58d420871938cdfd960c82fa0dd5a249
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-09-03 10:32:57 -0700

    structured

--- src/Day1.tsx
+++ src/Day1.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { View, Text,StyleSheet,Pressable, TextInput } from "react-native"
+import { View, Text, StyleSheet, Pressable, TextInput } from "react-native"
 
-export default function Day1(){
-    const[input,setInput] = React.useState('')
-    const[output,setOutput] = React.useState<number | string>('')
-   
+export default function Day1() {
+    const [input, setInput] = React.useState('')
+    const [output, setOutput] = React.useState<number | string>('')
+
     const Convert = () => {
         const number = Number(input)
         setOutput(number)
@@ -14,15 +14,15 @@ export default function Day1(){
         <View style={Styles.container}>
             <Text>Day1</Text>
             <TextInput
-            value = {input}
-            onChangeText = {setInput}
-            placeholder = "Enter a String"
+                value={input}
+                onChangeText={setInput}
+                placeholder="Enter a String"
             >
 
 
             </TextInput>
             <Pressable
-            onPress = {Convert}
+                onPress={Convert}
             >
                 <Text>Convert</Text>
 
@@ -34,11 +34,11 @@ export default function Day1(){
     )
 };
 
-const Styles = StyleSheet.create({   
-    container:{
+const Styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',      
+        alignItems: 'center',
     }
 });
 
