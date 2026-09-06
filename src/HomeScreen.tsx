6b0d3329cee230c1f2568e1527144abb58ea34be
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-09-06 03:07:41 -0700

    day5 added to home navigation

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -5,8 +5,8 @@ export default function HomeScreen({ navigation }: any) {
     return (
         <View style={Styles.container}>
             <Text>HomeScreen</Text>
-            <Pressable onPress={() => navigation.navigate("Day4")}>
-                <Text>Day 4</Text>
+            <Pressable onPress={() => navigation.navigate("Day5")}>
+                <Text>Day 5</Text>
             </Pressable>
         </View>
     )
