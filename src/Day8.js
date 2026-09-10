91a75a7de25a2cc4e6ffb08135b57aab9b17ed90
Awais-Chandio <94171625+Awais-Chandio@users.noreply.github.com>
2026-09-10 11:22:15 +0500

    hoisting concepts in js

---
+++ src/Day8.js
@@ -0,0 +1,21 @@
+
+//standard function or function declaration 
+//function declaration is hoisted 
+console.log(sum(2,4))
+function sum(a,b){
+return a+b
+}
+
+
+//function expression is not hoisted, arrow function is the shorter syntax for it 
+console.log(product(3,3))
+ const product = (a,b)=>{
+    return a*b
+
+ }
+
+
+
+
+
+
