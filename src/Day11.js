a17b17c353248b2e07093c8e22836fa73d968956
Awais-Chandio <94171625+Awais-Chandio@users.noreply.github.com>
2026-09-16 13:11:33 +0500

    smallerElement hunted from array

--- src/Day11.js
+++ src/Day11.js
@@ -1,5 +1,5 @@
 const greaterElement = (numArray)=>{
-let greaterCount = [0];
+let greaterCount = numArray[0];
 for(let i=0; i<numArray.length; i++){
     if(numArray[i]>greaterCount){
         greaterCount = numArray[i];
@@ -8,3 +8,15 @@ for(let i=0; i<numArray.length; i++){
 return greaterCount;
 }
 console.log(greaterElement([1,2,3,4,11,6,7,8,9,10]));
+
+
+const smallerElement = (num2Array)=>{
+    let smallerCount = num2Array[0];
+    for(let i=0; i<num2Array.length; i++){
+        if(num2Array[i]<smallerCount){
+            smallerCount = num2Array[i];
+        }
+    }
+    return smallerCount;
+}
+console.log(smallerElement([1,-7,2,3,4,11,6,7,8,9,10]));
